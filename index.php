8adc5962aa5e53cfd68e61e1038f276e81152bb3
Jean Gonçalves <jean@agenciasys.com.br>
2015-09-24 15:36:50 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>teste de locução</title>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body onload="init()">
+
+    <h2>Gravando o que estou dizendo</h2>
+
+    <button id="start">Iniciar</button>
+    <button id="stop">Parar</button><br><br>
+    <textarea id="text" style="width: 550px; height: 100px; font-size: 20px;" placeholder="Aguarde"></textarea>
+
+</body>
+</html>
