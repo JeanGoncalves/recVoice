fbfe82196f2d2961b9966a6f0eb15892c51a869e
Jean Gonçalves <jean@agenciasys.com.br>
2015-09-25 13:54:05 -0300

    edit components location

--- recvoice-1.0.js
+++ recvoice-1.0.js
@@ -5,59 +5,79 @@
  * Author: Jean Gonçalves
  * Email: jeanfpgoncalves@live.com
  *
- * Date: 2015-09-24
+ * Date: 2015-09-25
  */
 
-(function ( $ ) {
+(function($) {
 
-	$.fn.recVoice = function() {
+    $.fn.recVoice = function() {
+        verify();
 
-		var self = $(this.selector);
-		var spech = createSpeech(self);
+        var self = $(this);
 
-		var buttonStart = createButton('Iniciar');
-		buttonStart.addEventListener('click',function(){
-			spech.start();
-			buttonStart.disabled = true;
-			buttonStop.disabled = false;
-		});
-		
-		var buttonStop = createButton('Parar',true);
-		buttonStop.addEventListener('click',function(){
-			spech.stop();
-			buttonStart.disabled = false;
-			buttonStop.disabled = true;
-		});
+        var location;
+        for (var i = 0; i < self.parent().children().length; i++) {
+            if ($(self.parent().children()[i])[0] === self[0])
+                location = $(self.parent().children()[i - 1]);
+        }
+        console.log(location);
+        var spech = createSpeech(self);
+
+        var div = createDiv();
+
+        var buttonStart = createButton('Iniciar');
+        buttonStart.addEventListener('click', function() {
+            spech.start();
+            buttonStart.disabled = true;
+            buttonStop.disabled = false;
+        });
+
+        var buttonStop = createButton('Parar', true);
+        buttonStop.addEventListener('click', function() {
+            spech.stop();
+            buttonStart.disabled = false;
+            buttonStop.disabled = true;
+        });
 
 
-		self.parent().append(buttonStart);
-		self.parent().append(buttonStop);
-	};
+        location.append(buttonStart);
+        location.append(buttonStop);
+    };
 
-	var createButton = function(options,disabled) {
-		var btn = document.createElement('INPUT');
-		btn.setAttribute("type","button");
+    var createButton = function(options, disabled) {
+        var btn = document.createElement('INPUT');
+        btn.setAttribute("type", "button");
 
-		btn.value = 'Novo Botão';
-		if(options)
-			btn.value = options;
+        btn.value = 'Novo Botão';
+        if (options)
+            btn.value = options;
 
-		btn.disabled = false;
-		if(disabled)
-			btn.disabled = disabled;
+        btn.disabled = false;
+        if (disabled)
+            btn.disabled = disabled;
 
-		return btn;		
-	};
+        return btn;
+    };
 
-	var createSpeech = function(elem) {
-		var speech = new webkitSpeechRecognition;
-		speech.continuous = true;
-		speech.interimResults = true;
-		speech.lang = 'pt-BR';
-		speech.onresult = function(e) {
-			var text = e.results[e.results.length - 1][0].transcript;
-			elem.html(text);
-		}
-		return speech;
-	};
-}( jQuery ));
+    var createDiv = function() {
+        var div = document.createElement("DIV");
+        return div;
+    };
+
+    var createSpeech = function(elem) {
+        var speech = new webkitSpeechRecognition;
+        speech.continuous = true;
+        speech.interimResults = true;
+        speech.lang = navigator.language;
+        speech.onresult = function(e) {
+            var text = e.results[e.results.length - 1][0].transcript;
+            elem.html(text);
+        }
+        return speech;
+    };
+
+    var verify = function() {
+        if (typeof webkitSpeechRecognition !== 'function')
+            throw new Error("\n\nDon't have a function webkitSpeechRecognition in your navigator.\nNeed a use Chrome Browser.\n\n");
+    };
+}(jQuery));
