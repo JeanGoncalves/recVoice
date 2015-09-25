30c3d6e41f6cfdc6f6b23b957768a58270f65c62
Jean Gonçalves <jeanfpgoncalves@live.com>
2015-09-24 23:30:47 -0300

    Create the first version RecVoice

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>teste de locução</title>
+    <title>RecVoice 1.0</title>
     <meta charset="utf-8">
-    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="jquery/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="recvoice-1.0.js"></script>
 </head>
-<body onload="init()">
+<body>
 
     <h2>Gravando o que estou dizendo</h2>
 
-    <button id="start">Iniciar</button>
-    <button id="stop">Parar</button><br><br>
-    <textarea id="text" style="width: 550px; height: 100px; font-size: 20px;" placeholder="Aguarde"></textarea>
+    <textarea class="text" style="width: 550px; height: 100px; font-size: 20px;" placeholder="Aguarde"></textarea>
+
+	<script>
+		$(document).ready(function() {
+		    $('.text').recVoice();
+		});
+	</script>
 
 </body>
 </html>
---
+++ recvoice-1.0.js
@@ -0,0 +1,64 @@
+/*!
+ * RecVoice Javascript Chrome Library 1.0
+ * https://github.com/JeanGoncalves/recVoice
+ *
+ * Author: Jean Gonçalves
+ * Email: jeanfpgoncalves@live.com
+ *
+ * Date: 2015-09-24
+ */
+
+(function ( $ ) {
+
+	$.fn.recVoice = function() {
+
+		var self = $(this.selector);
+		var spech = createSpeech(self);
+
+		var buttonStart = createButton('Iniciar');
+		buttonStart.addEventListener('click',function(){
+			spech.start();
+			buttonStart.disabled = true;
+			buttonStop.disabled = false;
+		});
+		
+		var buttonStop = createButton('Parar',true);
+		buttonStop.addEventListener('click',function(){
+			spech.stop();
+			buttonStart.disabled = false;
+			buttonStop.disabled = true;
+		});
+
+
+		self.parent().append(buttonStart);
+		self.parent().append(buttonStop);
+	};
+
+	var createButton = function(options,disabled) {
+		var btn = document.createElement('INPUT');
+		btn.setAttribute("type","button");
+
+		btn.value = 'Novo Botão';
+		if(options)
+			btn.value = options;
+
+		btn.disabled = false;
+		if(disabled)
+			btn.disabled = disabled;
+
+		return btn;		
+	};
+
+	var createSpeech = function(elem) {
+		var speech = new webkitSpeechRecognition;
+		speech.continuous = true;
+		speech.interimResults = true;
+		speech.lang = 'pt-BR';
+		speech.onresult = function(e) {
+			var text = e.results[e.results.length - 1][0].transcript;
+			elem.html(text);
+		    console.log(text);
+		}
+		return speech;
+	};
+}( jQuery ));
